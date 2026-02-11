f0310e2dd24eff5e6e3532939f59398b5a611c91
HHOY <the.real.hhoy@gmail.com>
2026-02-11 17:07:39 +0200

    Added Downloading and Uploading to server

--- src/server/server.lua
+++ src/server/server.lua
@@ -1,10 +1,34 @@
 -- Config
 
 local SITE_FOLDER = "sites"
+local DOWNLOAD_FOLDER = "download"
+local UPLOAD_FOLDER = "upload"
+local UPLOAD_OVERWRITING = false
 
 -- end Config
 
 local modem = peripheral.find("modem")
+
+local function getDownload(file)
+    local path = fs.combine(DOWNLOAD_FOLDER,file)
+    if fs.exists(path) and not fs.isDir(path) then
+        local handle = fs.open(path,"r")
+        local data = handle.readAll()
+        handle.close()
+        return data
+    else
+        return nil
+    end
+end
+
+local function upload(name,data)
+    local path = fs.combine(UPLOAD_FOLDER,name)
+    if fs.exists(path) and not UPLOAD_OVERWRITING then return end
+    local handle = fs.open(path,"w")
+    handle.write(data)
+    handle.close()
+    return true
+end
 
 local function getPage(page)
     local path = fs.combine(SITE_FOLDER,page)
@@ -47,7 +71,26 @@ while true do
                     }
                 })
             elseif pack.action == "download" then
-                
+                local file = getDownload(pack.file)
+
+                modem.transmit(80,80,{
+                    ["destination"] = "CLIENT",
+                    ["id"] = pack.id,
+                    ["reply"] = {
+                        ["success"] = file == true,
+                        ["data"] = file
+                    }
+                })
+            elseif pack.action == "upload" then
+                local status = upload(pack.file,pack.data)
+
+                modem.transmit(80,80,{
+                    ["destination"] = "CLIENT",
+                    ["id"] = pack.id,
+                    ["reply"] = {
+                        ["success"] = status
+                    }
+                })
             end
         end
     end

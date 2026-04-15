e22f8bdca49d5535e7a7392ddb7e7dbf2d737f6e
THEHHOY <121125394+THEHHOY@users.noreply.github.com>
2026-04-15 15:49:41 +0300

    This borke piece of shish.
    I need to add this file manually at main

---
+++ src/client/mipProtocol.lua
@@ -0,0 +1,82 @@
+local protocol = {}
+
+protocol.modem = peripheral.find("modem")
+
+if not protocol.modem then
+    error("Modem not available.",0)
+end
+
+local function openPort()
+    if not protocol.modem.isOpen(80) then protocol.modem.open(80) end
+end
+
+function protocol.dnsLookup(addr)
+    local id = math.random(1000,9999)
+
+    local payload = {
+        ["destination"] = "DNS",
+        ["action"] = "get_domain",
+        ["arg"] = addr,
+        ["id"] = id
+    }
+
+    protocol.modem.transmit(80,80,payload)
+
+    openPort()
+
+    local timeout = os.startTimer(10)
+
+    while true do
+        local ev = {os.pullEvent()}
+        if ev[1] == "modem_message" then
+            if type(ev[5]) == "table" then
+                if ev[5].destination == "CLIENT" then
+                    if ev[5].id == id then
+                        local reply = ev[5].reply
+                        return reply.success and reply.pcId or nil, "Address cannot be lookuped"
+                    end
+                end
+            end
+        elseif ev[1] == "timer" and ev[2] == timeout then
+            return nil, "DNS Unreachable"
+        end
+    end
+
+end
+
+function protocol.get_page(pcid,page)
+    local id = math.random(1000,9999)
+
+    local payload = {
+        ["destination"] = "SERVER",
+        ["action"] = "get_page",
+        ["pcid"] = pcid,
+        ["id"] = id,
+        ["page"] = page,
+    }
+
+    protocol.modem.transmit(80,80,payload)
+
+    openPort()
+
+    local timeout = os.startTimer(10)
+
+    while true do
+        local ev = {os.pullEvent()}
+        if ev[1] == "modem_message" then
+            if type(ev[5]) == "table" then
+                if ev[5].destination == "CLIENT" then
+                    if ev[5].id == id then
+                        local reply = ev[5].reply
+                        return reply.success and reply.data or nil, "Attempt not succesful"
+                    end
+                end
+            end
+        elseif ev[1] == "timer" and ev[2] == timeout then
+            return nil, "Webserver Unreachable"
+        end
+    end
+
+end
+
+return protocol
